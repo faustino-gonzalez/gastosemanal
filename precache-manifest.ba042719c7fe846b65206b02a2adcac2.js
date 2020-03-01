e99c38cc5c2405ab5cc77598878a429b72327e54
faustino-gonzalez <abcdfaustino@gmail.com>
2020-03-01 10:25:20 -0600

    Updates

---
+++ precache-manifest.ba042719c7fe846b65206b02a2adcac2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3cde53279235570b064be7e4938a7a",
+    "url": "/gastosemanal/index.html"
+  },
+  {
+    "revision": "9c150cffea9cf4a46b49",
+    "url": "/gastosemanal/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b592ad4a099c0ca5f499",
+    "url": "/gastosemanal/static/js/2.61a6ac2f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/gastosemanal/static/js/2.61a6ac2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c150cffea9cf4a46b49",
+    "url": "/gastosemanal/static/js/main.df02d173.chunk.js"
+  },
+  {
+    "revision": "a2f2df8bbb8fbc30b3c4",
+    "url": "/gastosemanal/static/js/runtime-main.fba0d20b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gastosemanal/static/media/logo.5d5d9eef.svg"
+  }
+]);
